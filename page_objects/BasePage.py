f3cbbe3c7132c7c562c1ff377cf308deac1f7033
elkoltsova <elkoltsova@ozon.ru>
2022-03-27 12:38:40 +0300

    hw 6 test  v2

--- page_objects/BasePage.py
+++ page_objects/BasePage.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.alert import Alert
@@ -10,24 +11,34 @@ class BasePage:
     def __init__(self, browser):
         self.browser = browser
 
+    @allure.step("Проверка наличия элемента {locator} на странице.")
     def verify_element_presence(self, locator: tuple):
         try:
             return WebDriverWait(self.browser, 5).until(EC.visibility_of_element_located(locator))
         except TimeoutException:
+            allure.attach(name=self.browser.session_id,
+                          body=self.browser.get_screenshot_as_png(),
+                          attachment_type=allure.attachment_type.PNG
+                          )
             raise AssertionError("Cant find element by locator: {}".format(locator))
 
+    @allure.step("Получаем текст элемента {locator}.")
     def get_element_text(self, locator: tuple):
         return self.browser.find_element(*locator).text
 
+    @allure.step("Заполняем поле {locator}.")
     def fill_form(self, locator: tuple, text: str):
         self.browser.find_element(*locator).send_keys(text)
 
+    @allure.step("Клик по полю {locator}.")
     def click_element(self, locator: tuple):
         ActionChains(self.browser).pause(2).move_to_element(self.browser.find_element(*locator)).click().perform()
 
+    @allure.step("Получение всех элементов по локатору {locator}.")
     def get_elements(self, locator: tuple):
         self.verify_element_presence(locator)
         return self.browser.find_elements(*locator)
 
+    @allure.step("Подтверждение алерта.")
     def accept_alert(self):
         Alert(self.browser).accept()
--- tests/test_opencart.py
+++ tests/test_opencart.py
@@ -1,5 +1,5 @@
 import random
-
+import allure
 import pytest
 
 from page_objects.AdminLoginPage import AdminLoginPage
@@ -9,58 +9,64 @@ from page_objects.ItemPage import ItemPage
 from page_objects.MainPage import MainPage
 from page_objects.RegisterPage import RegisterPage
 
-
+@allure.description("""Проверка наличия элементов на странице каталога""")
 def test_catalog(browser):
+    page = CatalogPage(browser)
     browser.open(CatalogPage.DESKTOP_PATH)
     page_elements = [CatalogPage.ITEM_LABEL, CatalogPage.ITEM_HEADER, CatalogPage.DESCRIPTION_IMAGE,
                      CatalogPage.DESCRIPTION_TEXT, CatalogPage.REFINE_SEARCH, CatalogPage.LIST_VIEW,
                      CatalogPage.GRID_VIEW]
     for el in page_elements:
-        CatalogPage(browser).verify_element_presence(el)
+        page.verify_element_presence(el)
 
-
+@allure.description("""Проверка наличия элементов на странице регистрации нового пользователя""")
 def test_register_page(browser):
+    page = RegisterPage(browser)
     browser.open(RegisterPage.REGISTER_PATH)
     page_elements = [RegisterPage.PAGE_HEADER, RegisterPage.HEADER_PERSONAL_DETAILS, RegisterPage.FIRST_NAME_FIELD,
                      RegisterPage.LAST_NAME_FIELD, RegisterPage.EMAIL_FIELD, RegisterPage.PHONE_FIELD,
                      RegisterPage.PASSWORD_HEADER, RegisterPage.PASSWORD_FIELD, RegisterPage.PASS_CONFIRM_FIELD,
                      RegisterPage.LETTER_HEADER, RegisterPage.RADIO_BUTTON_NO]
     for el in page_elements:
-        RegisterPage(browser).verify_element_presence(el)
+        page.verify_element_presence(el)
 
-
+@allure.description("""Проверка наличия элементов на главной странице""")
 def test_main_page(browser):
+    page = MainPage(browser)
     browser.open()
     page_elements = [MainPage.SEARCH_FIELD, MainPage.SEARCH_BUTTON, MainPage.CONTACT_PHONE,
                      MainPage.CURRENCY_PICKER, MainPage.MACBOOK_IMAGE]
     for el in page_elements:
-        MainPage(browser).verify_element_presence(el)
+        page.verify_element_presence(el)
 
-
+@allure.description("""Проверка наличия элементов на странице авторизации в раздел админитсратора""")
 def test_admin_login_page(browser):
+    page = AdminLoginPage(browser)
     browser.open(AdminLoginPage.ADMIN_PATH)
     page_elements = [AdminLoginPage.HEADER_TEXT, AdminLoginPage.USERNAME_FIELD, AdminLoginPage.PASSWORD_FIELD,
                      AdminLoginPage.FORGOTTEN_PASSWORD, AdminLoginPage.SUBMIT_BUTTON]
     for el in page_elements:
-        AdminLoginPage(browser).verify_element_presence(el)
+        page.verify_element_presence(el)
 
-
+@allure.description("""Проверка наличия элементов на странице товаров""")
 def test_item_page(browser):
+    page = ItemPage(browser)
     browser.open(ItemPage.IPHONE_ITEM_PATH)
     page_elements = [ItemPage.ITEM_HEADER, ItemPage.ITEM_IMG, ItemPage.QUANTITY_FIELD, ItemPage.ADD_ITEM_BUTTON,
                      ItemPage.DESCRIPTION_TAB, ItemPage.REVIEWS_TAB]
     for el in page_elements:
-        ItemPage(browser).verify_element_presence(el)
+        page.verify_element_presence(el)
 
-
+@allure.description("""Добавление нового продукта в разделе администратора""")
 def test_add_new_product(browser):
     browser.open(AdminLoginPage.ADMIN_PATH)
     AdminLoginPage(browser).login_to_admin()
-    AdminPage(browser).click_element(AdminPage.CATALOG_MENU)
-    AdminPage(browser).click_element(AdminPage.PRODUCTS_MENU)
-    assert AdminPage(browser).get_element_text(AdminPage.PAGE_HEADER) == 'Products'
-    AdminPage(browser).click_element(AdminPage.ADD_PRODUCT_BTN)
-    assert AdminPage(browser).get_element_text(AdminPage.PANEL_TITLE) == 'Add Product'
+    page = AdminPage(browser)
+    page.click_element(AdminPage.CATALOG_MENU)
+    page.click_element(AdminPage.PRODUCTS_MENU)
+    assert page.get_element_text(AdminPage.PAGE_HEADER) == 'Products'
+    page.click_element(AdminPage.ADD_PRODUCT_BTN)
+    assert page.get_element_text(AdminPage.PANEL_TITLE) == 'Add Product'
     product_info = {
         'name': 'New test product!',
         'tag': 'testing_tag',
@@ -68,16 +74,17 @@ def test_add_new_product(browser):
         'price': 100500.12
     }
     create_product(browser, product_info)
-    assert 'Success' in AdminPage(browser).get_element_text(AdminPage.ALERT_SUCCESS)
+    assert 'Success' in page.get_element_text(AdminPage.ALERT_SUCCESS)
     filter_product(browser, product_info['name'])
 
-
+@allure.description("""Удаление продукта в разделе администратора""")
 def test_delete_product(browser):
     browser.open(AdminLoginPage.ADMIN_PATH)
     AdminLoginPage(browser).login_to_admin()
-    AdminPage(browser).click_element(AdminPage.CATALOG_MENU)
-    AdminPage(browser).click_element(AdminPage.PRODUCTS_MENU)
-    AdminPage(browser).click_element(AdminPage.ADD_PRODUCT_BTN)
+    page = AdminPage(browser)
+    page.click_element(AdminPage.CATALOG_MENU)
+    page.click_element(AdminPage.PRODUCTS_MENU)
+    page.click_element(AdminPage.ADD_PRODUCT_BTN)
     product_info = {
         'name': 'New product to delete',
         'tag': 'testing_tag_del',
@@ -86,62 +93,57 @@ def test_delete_product(browser):
     }
     create_product(browser, product_info)
     filter_product(browser, product_info['name'])
-    AdminPage(browser).click_element(AdminPage.CHECK_PRODUCT)
-    AdminPage(browser).click_element(AdminPage.DELETE_PRODUCT_BTN)
-    AdminPage(browser).accept_alert()
-    assert 'Success' in AdminPage(browser).get_element_text(AdminPage.ALERT_SUCCESS)
+    page.click_element(AdminPage.CHECK_PRODUCT)
+    page.click_element(AdminPage.DELETE_PRODUCT_BTN)
+    page.accept_alert()
+    assert 'Success' in page.get_element_text(AdminPage.ALERT_SUCCESS)
 
-
-@pytest.mark.parametrize('currency', ['EURO', 'POUND', 'DOLLAR'])
-def test_change_currency(browser, currency):
+@allure.description("""Иземенение валюты через выпадающий список""")
+@pytest.mark.parametrize('cur_button, cur_text, cur_sign', [(MainPage.CURRENCY_EURO, '€ Euro', '€'),
+                                                            (MainPage.CURRENCY_POUND, '£ Pound Sterling', '£'),
+                                                            (MainPage.CURRENCY_DOLLAR, '$ US Dollar', '$')])
+def test_change_currency(browser, cur_button, cur_text, cur_sign):
     browser.open()
-    MainPage(browser).click_element(MainPage.CURRENCY_PICKER)
-    if currency == 'EURO':
-        cur_button = MainPage.CURRENCY_EURO
-        cur_text = '€ Euro'
-        cur_sign = '€'
-    elif currency == 'POUND':
-        cur_button = MainPage.CURRENCY_POUND
-        cur_text = '£ Pound Sterling'
-        cur_sign = '£'
-    elif currency == 'DOLLAR':
-        cur_button = MainPage.CURRENCY_DOLLAR
-        cur_text = '$ US Dollar'
-        cur_sign = '$'
-    assert MainPage(browser).get_element_text(cur_button) == cur_text
-    MainPage(browser).click_element(cur_button)
-    assert MainPage(browser).get_element_text(MainPage.CURRENCY_SIGN) == cur_sign
-    assert cur_sign in MainPage(browser).get_element_text(MainPage.CART_TOTAL)
-    for el in MainPage(browser).get_elements(MainPage.ITEMS_PRICE):
+    page = MainPage(browser)
+    page.click_element(MainPage.CURRENCY_PICKER)
+    assert page.get_element_text(cur_button) == cur_text
+    page.click_element(cur_button)
+    assert page.get_element_text(MainPage.CURRENCY_SIGN) == cur_sign
+    assert cur_sign in page.get_element_text(MainPage.CART_TOTAL)
+    for el in page.get_elements(MainPage.ITEMS_PRICE):
         assert cur_sign in el.text
 
 
+@allure.description("""Добавление нового пользователя""")
 def test_user_register(browser):
     browser.open(RegisterPage.REGISTER_PATH)
-    assert RegisterPage(browser).get_element_text(RegisterPage.PAGE_HEADER) == 'Register Account'
-    RegisterPage(browser).fill_form(RegisterPage.FIRST_NAME_FIELD, 'test_first_name')
-    RegisterPage(browser).fill_form(RegisterPage.LAST_NAME_FIELD, 'test_last_name')
-    RegisterPage(browser).fill_form(RegisterPage.EMAIL_FIELD,
+    page = RegisterPage(browser)
+    assert page.get_element_text(RegisterPage.PAGE_HEADER) == 'Register Account'
+    page.fill_form(RegisterPage.FIRST_NAME_FIELD, 'test_first_name')
+    page.fill_form(RegisterPage.LAST_NAME_FIELD, 'test_last_name')
+    page.fill_form(RegisterPage.EMAIL_FIELD,
                                     'test_user_' + str(random.randint(1, 10000)) + '@gmail.com')
-    RegisterPage(browser).fill_form(RegisterPage.PHONE_FIELD, '89997775634')
-    RegisterPage(browser).fill_form(RegisterPage.PASSWORD_FIELD, '1234567890')
-    RegisterPage(browser).fill_form(RegisterPage.PASS_CONFIRM_FIELD, '1234567890')
-    RegisterPage(browser).click_element(RegisterPage.RADIO_BUTTON_YES)
-    RegisterPage(browser).click_element(RegisterPage.POLICY_CHECKBOX)
-    RegisterPage(browser).click_element(RegisterPage.CONTINUE_BUTTON)
-    assert RegisterPage(browser).get_element_text(RegisterPage.PAGE_HEADER) == 'Your Account Has Been Created!'
+    page.fill_form(RegisterPage.PHONE_FIELD, '89997775634')
+    page.fill_form(RegisterPage.PASSWORD_FIELD, '1234567890')
+    page.fill_form(RegisterPage.PASS_CONFIRM_FIELD, '1234567890')
+    page.click_element(RegisterPage.RADIO_BUTTON_YES)
+    page.click_element(RegisterPage.POLICY_CHECKBOX)
+    page.click_element(RegisterPage.CONTINUE_BUTTON)
+    assert page.get_element_text(RegisterPage.PAGE_HEADER) == 'Your Account Has Been Created!'
 
 
 def create_product(browser, product: dict):
-    AdminPage(browser).fill_form(AdminPage.PRODUCT_NAME_FIELD, product['name'])
-    AdminPage(browser).fill_form(AdminPage.TAG_TITLE_FIELD, product['tag'])
-    AdminPage(browser).click_element(AdminPage.DATA_TAB)
-    AdminPage(browser).fill_form(AdminPage.MODEL_FIELD, product['model'])
-    AdminPage(browser).fill_form(AdminPage.PRICE_FIELD, product['price'])
-    AdminPage(browser).click_element(AdminPage.SAVE_BTN)
+    page = AdminPage(browser)
+    page.fill_form(AdminPage.PRODUCT_NAME_FIELD, product['name'])
+    page.fill_form(AdminPage.TAG_TITLE_FIELD, product['tag'])
+    page.click_element(AdminPage.DATA_TAB)
+    page.fill_form(AdminPage.MODEL_FIELD, product['model'])
+    page.fill_form(AdminPage.PRICE_FIELD, product['price'])
+    page.click_element(AdminPage.SAVE_BTN)
 
 
 def filter_product(browser, product_name):
-    AdminPage(browser).fill_form(AdminPage.PRODUCT_NAME_FIELD, product_name)
-    AdminPage(browser).click_element(AdminPage.FILTER_BUTTON)
-    assert product_name in AdminPage(browser).get_element_text(AdminPage.TABLE_PRODUCT_NAME)
+    page = AdminPage(browser)
+    page.fill_form(AdminPage.PRODUCT_NAME_FIELD, product_name)
+    page.click_element(AdminPage.FILTER_BUTTON)
+    assert product_name in page.get_element_text(AdminPage.TABLE_PRODUCT_NAME)
